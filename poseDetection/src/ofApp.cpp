caa5b1a852a48d09fcb7815d32e0f712c92c52cb
Gerben Neven <gerbyzation@me.com>
2015-05-30 00:28:21 +0100

    convert source files from tabs to spaces

--- poseDetection/src/ofApp.cpp
+++ poseDetection/src/ofApp.cpp
@@ -4,108 +4,110 @@ const string ofApp::joints[] = { "SpineBase", "SpineMid", "Neck", "Head", "Shoul
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	kinect.open();
-	kinect.initBodySource();
-	kinect.initColorSource();
-	kinect.initDepthSource();
+    kinect.open();
+    kinect.initBodySource();
+    kinect.initColorSource();
+    kinect.initDepthSource();
 
-	camera.setDistance(10);
+    camera.setDistance(10);
 
-	ofSetWindowShape(1920, 1080);
+    ofSetWindowShape(1920, 1080);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-	kinect.update();
+    kinect.update();
 
-	// mesh = kinect.getDepthSource()->getMesh(
-	//	false, 
-//		ofxKinectForWindows2::Source::Depth::PointCloudOptions::TextureCoordinates::ColorCamera);
+    // mesh = kinect.getDepthSource()->getMesh(
+    //  false, 
+//      ofxKinectForWindows2::Source::Depth::PointCloudOptions::TextureCoordinates::ColorCamera);
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	ofBackground(75, 95, 115);
+    ofBackground(75, 95, 115);
 
-	ofSetColor(255);
-	kinect.getColorSource()->draw(0, 0, 320, 180);
+    ofSetColor(255);
+    kinect.getColorSource()->draw(0, 0, 320, 180);
 
-	camera.begin();
+    camera.begin();
 
-	ofPushStyle();
-		ofScale(10, 10, -10);
+    ofPushStyle();
+        ofScale(10, 10, -10);
 
-		// mesh.draw();
+        // mesh.draw();
 
-		drawJoints3D();
-	ofPopStyle();
+        drawJoints3D();
+    ofPopStyle();
 
-	camera.end();
+    camera.end();
 
-	ofDrawBitmapString(ofToString(ofGetFrameRate()), ofGetWidth() - 100, 50);
+    ofDrawBitmapString(ofToString(ofGetFrameRate()), ofGetWidth() - 100, 50);
 }
 
 void ofApp::drawJoints3D() {
-	// DRAW THE JOINTS IN A SALMON COLOR
-	ofVec3f pos;
+    // DRAW THE JOINTS IN A SALMON COLOR
+    ofVec3f pos;
 
 
-	string location;
-	// THERE IS A MAXIMUM OF 6 BODIES TRACKED BY KINECT
-	for (int i = 0; i<6; i++){
+    string location;
+    // THERE IS A MAXIMUM OF 6 BODIES TRACKED BY KINECT
+    for (int i = 0; i<6; i++){
 
-		// IF THE BODY IS BEING TRACKED...
-		if (this->kinect.getBodySource()->getBodies()[i].tracked){
+        // IF THE BODY IS BEING TRACKED...
+        if (this->kinect.getBodySource()->getBodies()[i].tracked){
 
-			auto b = this->kinect.getBodySource()->getBodies()[i];
-			
-			std::map<JointType, ofxKFW2::Data::Joint>::iterator it;
-			// ITERATE THROUGH ALL JOINTS IN THE TRACKED BODY...
-			for (it = b.joints.begin(); it != b.joints.end(); ++it)
-			{
-				if (it->second.getTrackingState() == TrackingState_Tracked)
-				{
-					// GRAB THE JOINT'S 3D POSITION
-					pos = it->second.getPosition();
-					// draw box on position
-					// give every body diff joint color
-					ofSetColor(ofColor::fromHsb(i*40, 255, 255));
-					ofBox(pos.x, pos.y, pos.z, .05, .05, .05);
+            auto b = this->kinect.getBodySource()->getBodies()[i];
+            
+            std::map<JointType, ofxKFW2::Data::Joint>::iterator it;
+            // ITERATE THROUGH ALL JOINTS IN THE TRACKED BODY...
+            for (it = b.joints.begin(); it != b.joints.end(); ++it)
+            {
+                if (it->second.getTrackingState() == TrackingState_Tracked)
+                {
+                    // GRAB THE JOINT'S 3D POSITION
+                    pos = it->second.getPosition();
+                    // draw box on position
+                    // give every body diff joint color
+                    ofSetColor(ofColor::fromHsb(i*40, 255, 255));
+                    ofBox(pos.x, pos.y, pos.z, .05, .05, .05);
 
-					ofSetColor(255);
-					ofDrawBitmapString(joints[it->first], pos.x, pos.y, pos.z);
+                    ofSetColor(255);
+                    ofDrawBitmapString(joints[it->first], pos.x, pos.y, pos.z);
 
-					if (it->first == ElbowRight) {
-						std::map<JointType, ofxKFW2::Data::Joint>::iterator shoulder = it;
-						std::map<JointType, ofxKFW2::Data::Joint>::iterator wrist = it;
+                    if (it->first == ElbowRight) {
+                        // it works, but doesn't seem a clean solution to me...
+                        std::map<JointType, ofxKFW2::Data::Joint>::iterator shoulder = it;
+                        std::map<JointType, ofxKFW2::Data::Joint>::iterator wrist = it;
 
-						--shoulder;
-						++wrist;
-						
-						ofPoint posShoulder = shoulder->second.getPosition();
-						ofPoint posElbow = it->second.getPosition();
-						ofPoint posWrist = wrist->second.getPosition();
+                        --shoulder;
+                        ++wrist;
+                        
+                        ofPoint posShoulder = shoulder->second.getPosition();
+                        ofPoint posElbow = it->second.getPosition();
+                        ofPoint posWrist = wrist->second.getPosition();
 
-						// calculate distances between points
-						float distSW = sqrtf(pow(posShoulder.x - posWrist.x, 2) + pow(posShoulder.y - posWrist.y, 2));
-						float distSE = sqrtf(pow(posShoulder.x - posElbow.x, 2) + pow(posShoulder.y - posElbow.y, 2));
-						float distEW = sqrtf(pow(posElbow.x - posWrist.x, 2) + pow(posElbow.y - posWrist.y, 2));
+                        // calculate distances between points
+                        float distSW = sqrtf(pow(posShoulder.x - posWrist.x, 2) + pow(posShoulder.y - posWrist.y, 2));
+                        float distSE = sqrtf(pow(posShoulder.x - posElbow.x, 2) + pow(posShoulder.y - posElbow.y, 2));
+                        float distEW = sqrtf(pow(posElbow.x - posWrist.x, 2) + pow(posElbow.y - posWrist.y, 2));
 
-						// sinusregel c^2 = a^2 + b^2 - 2ab * cos(C)
-						// cos(C) = (a^2 + b^2 - c^2) / 2ab
-						float angle = acosf((pow(distSE, 2) + pow(distEW, 2) - pow(distSW, 2)) / (2 * distSE * distEW));
-						// angle is in radians
-						cout << angle << endl;
+                        // cosine rule c^2 = a^2 + b^2 - 2ab * cos(C)
+                        // cos(C) = (a^2 + b^2 - c^2) / 2ab
+                        float angle = acosf((pow(distSE, 2) + pow(distEW, 2) - pow(distSW, 2)) / (2 * distSE * distEW));
+                        // angle is in radians
+                        // rotation is over z-axis?
+                        // cout << ofRadToDeg(angle) << endl;
 
-						// get the angle of elbow
-					}
+                    } else if (it->first == ShoulderRight) {
+                        
+                    }
+                }
 
-				}
-
-			}
-		}
-	}
-	ofSetColor(255);
+            }
+        }
+    }
+    ofSetColor(255);
 }
 
 //--------------------------------------------------------------
